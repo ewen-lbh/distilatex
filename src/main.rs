ccb1337b10a57e8f3cb17b2509731c66c8133d51
Ewen Le Bihan <hey@ewen.works>
2021-04-16 10:22:38 +0200

    🚧 Make Cli struct, read from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Cli {
+    /// The file to summarize
+    #[structopt(parse(from_os_str))]
+    file: std::path::PathBuf,
+}
+
+
 fn main() {
-	
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.file)
+        .expect(&format!("Couldn't read file {:?}", &args.file)[..]);
+    println!("{}", &content);
 }
 
